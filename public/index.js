b239e30ec4692da8977204ee89abe9805a389c24
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-09-16 18:06:40 +0530

    added basic effects

---
+++ public/index.js
@@ -0,0 +1,11 @@
+var image = document.querySelectorAll("#imageStyle");
+
+for(var i = 0 ; i<image.length;i++)
+{
+image[i].addEventListener("mouseover",function(){
+  this.style.width = "100%";
+});
+image[i].addEventListener("mouseout",function(){
+  this.style.width = "70%";
+});
+}
