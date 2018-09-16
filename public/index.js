b2bf42f486f88d3333d81d3ef154619e5dd53149
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-09-16 18:45:22 +0530

    add transparency effect

--- public/index.js
+++ public/index.js
@@ -4,9 +4,13 @@ for(var i = 0 ; i<image.length;i++)
 {
 image[i].addEventListener("mouseover",function(){
   this.style.width = "75%";
+  this.style.opacity ="0.2";
+  this.style.filter ="alpha(opacity=50%)";
 });
 image[i].addEventListener("mouseout",function(){
   this.style.width = "70%";
   this.style.transition = "1s";
+  this.style.opacity ="0.9";
+
 });
 }
